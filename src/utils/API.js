c1db108948b495c63f8efe13a3082aa592a2bb12
Sofia <sofimantas@gmail.com>
2021-02-26 16:44:11 -0500

    fixed API route that was only producing one result

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = {
     //gets all users
     getUsers: function() {
-        return axios.get('https://randomuser.me/api/?results=200?nat=us');
+        return axios.get('https://randomuser.me/api/?results=25&seed=seed');
     }
 };
 export default API;
