74c678a173c55f01fa2906fde856ff11610e1868
Sofia <sofimantas@gmail.com>
2021-02-26 16:52:56 -0500

    increased API number

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = {
     //gets all users
     getUsers: function() {
-        return axios.get('https://randomuser.me/api/?results=25&seed=seed');
+        return axios.get('https://randomuser.me/api/?results=55&seed=seed');
     }
 };
 export default API;
